2ef2f5b1377a721b3f1a85f71caff6cdd24f3c76
AbrGarcia01 <161896847+AbrGarcia01@users.noreply.github.com>
2024-09-13 22:07:08 -0300

    Add files via upload

---
+++ cjoweb2/central/fn/menu.php
@@ -0,0 +1,14 @@
+<?php
+
+$menu_links_clientes = "
+	<div class='topnav' id='myTopnav'>
+	  <a href='index2.php' class='active'>Home</a>
+	  <a href='cliente_cadastrar.php'>Cadastro Cliente</a>
+	  <a href='clientes_listar.php'>Listar Clientes</a>
+	  <a href='usuario_cadastrar.php'>Cadastrar Usuário</a>
+	  <a href='javascript:void(0);' class='icon' onclick='myFunction()'>
+	    <i class='fa fa-bars'></i>
+	  </a>
+	</div>
+";
+?>
